202b9852996b7ca0fa864bb1eaed6eca622a4be1
scjqwe <2290676174@qq.com>
2019-04-22 23:40:12 +0800

    项目创建

---
+++ src/main/java/com/gateway/SpringCloudGatewayExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayExamplesApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringCloudGatewayExamplesApplication.class, args);
+	}
+
+}
